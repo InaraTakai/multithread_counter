9a6d6e76bfe3d425b3ffefbe40f3fba60737b058
Inara <inaratakashi@gmail.com>
2020-08-09 03:09:04 -0300

    Tudo funcionando

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 int qtd_primos;
 int numero;
 char caracter;
-pthread_mutex_t trava;
 
 int num_primo(int x){ /* Função que determina se número é primo */
   int i, num_dividores;
@@ -27,64 +26,53 @@ int num_primo(int x){ /* Função que determina se número é primo */
   }
 }
 
-void* funcao_thread(void *arg) {
+void* funcao_thread(void *arg) { /* No parâmetro *arg é passado o inteiro que será verificado */
   int* N = (int*)arg;
   int M = (*N);
 
-  printf("Estou na thread %d!\n", M);
+  for (unsigned int j=0; j<5000000; j++); /* Pode ser trocado pelo sleep, porém aumenta tempo de execução */
+  /* sleep(1); */
 
-  pthread_mutex_lock(&trava);
+  qtd_primos += num_primo(M);
 
-  // printf("Numero %d!\n", numero);
-  qtd_primos += num_primo(numero);
-
-  pthread_mutex_unlock(&trava);
-
-  printf("Saindo da thread %d!\n", M);
   return NULL;
 }
 
 int main() {
   pthread_t threads[4];
-  int thread_id[4];
   int aux = 0;
   int num_processos = 0;
 
-  qtd_primos =0;
-
-  for (int i = 0; i < 4; i++) {
-    thread_id[i] = i;
-  }
+  qtd_primos = 0;
 
   while (aux == 0){
-    if (num_processos < 4){
-      pthread_mutex_lock(&trava);
+    if (num_processos < 4){  /* Se o numero de threads é menor que 4, realiza a leitura da entrada e cria uma thread */
       scanf("%d%c", &numero, &caracter);
-      // printf("Numero lido %d!\n", numero);
 
-      pthread_create(&(threads[num_processos]), NULL, funcao_thread, &(thread_id[num_processos]));
+      pthread_create(&(threads[num_processos]), NULL, funcao_thread, &numero);
 
-      pthread_mutex_unlock(&trava);
-      for (int p = 0; p<800000; p++);
+      for (int p = 0; p<800000; p++); /* Pode ser trocado pelo sleep, porém aumenta tempo de execução */
+      /* sleep(1); */
+
       num_processos++;
 
       if (caracter == '\n'){
         aux = 1;
         break;
       }
-    }else {
+    }else { /* Caso o número de threads abertas seja 4, aguarda até uma thread terminar */
       pthread_join(threads[num_processos-1], NULL);
       num_processos--;
     }
   }
 
 
-  /* Esperando threads terminarem! */
+  /* Esperando todas as threads terminarem */
   for (int i = 0; i < 4; i++) {
     pthread_join(threads[i], NULL);
   }
 
-  printf("Primos: %d!\n", qtd_primos);
+  printf("%d\n", qtd_primos);
 
   return 0;
 }
